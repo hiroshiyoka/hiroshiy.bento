8af0ecb60faa93cb34058622627434f692a9ef80
hiroshiy <fadilahsyafaat2427@gmail.com>
2023-08-09 18:02:08 +0800

    creating layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,17 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-4xl font-bold">Raka Fadilah</h1>
-      {/* <ThemeToggle /> */}
+    <main className="flex flex-1 h-full w-full gap-10">
+      <div className="flex-1 p-6 h-full max-w-md">
+        <div className="flex items-center justify-center h-full rounded-md dark:bg-emerald-700">
+          Right Side
+        </div>
+      </div>
+      <div className="flex-1 p-6 h-full">
+        <div className="flex items-center justify-center h-full rounded-md dark:bg-emerald-700">
+          Left Side
+        </div>
+      </div>
     </main>
   )
 }
