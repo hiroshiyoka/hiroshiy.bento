fe33aec8e865eb97c28a9a35cde4bc5790feb336
hiroshiy <fadilahsyafaat2427@gmail.com>
2023-08-09 16:28:55 +0800

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+// import { ThemeToggle } from '@/components/theme.toggle'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-4xl font-bold">Raka Fadilah</h1>
+      {/* <ThemeToggle /> */}
+    </main>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./components/**/*.{js,ts,jsx,tsx,mdx}",
+      "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    darkMode : ["class"],
+    theme: {
+      extend: {
+        colors: {
+          primary: "#6178F1",
+          youtube: "#FF0000",
+          twitter: "#1DA1F2",
+          discord: "#5865F2",
+          github: "#181717",
+        },
+        boxShadow: {
+          grid: "0px 4px 10px 0px rgba(0, 0, 0, 0.05);",
+        },
+        fontFamily: {
+          sans: ("var(--font-sfpro)"),
+        },
+      },
+    },
+    plugins: [],
+};
