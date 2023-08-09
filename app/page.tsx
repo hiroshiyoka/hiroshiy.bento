09209a7cda54d076993d08a155ae2f99cd4c666a
hiroshiy <fadilahsyafaat2427@gmail.com>
2023-08-09 18:25:55 +0800

    create left-side

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,31 @@
 // import { ThemeToggle } from '@/components/theme.toggle'
+import { siteConfig } from '@/config/site.config'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="flex flex-1 h-full w-full gap-10">
       <div className="flex-1 p-6 h-full max-w-md">
-        <div className="flex items-center justify-center h-full rounded-md dark:bg-emerald-700">
-          Left Side
+        <div className="flex flex-col h-full rounded-md space-y-6">
+          <img 
+            className="rounded-full" 
+            src="about.jpg" 
+            alt="raka-fadilah" 
+            width="90" 
+             height="90"
+            loading="eager"
+          />
+          <div>
+            <div className="text-xl text-primary font-semibold">
+              {siteConfig.title}
+            </div>
+            <div>
+              {siteConfig.creator}
+            </div>
+            <div>
+              {siteConfig.bio}
+            </div>
+          </div>
         </div>
       </div>
       <div className="flex-1 p-6 h-full">
---
+++ config/site.config.ts
@@ -0,0 +1,8 @@
+export const siteConfig = {
+    creator: "Raka Fadilah",
+    title: "Frontend Developer",
+    bio: "Building Website and creating Application",
+    location: "Berau, Indonesia",
+    locationLink: "https://goo.gl/maps/jdFYax1KvdpduGtS9",
+    email: "fadilahsyafaat2427@gmail.com"
+} as const;
