a4e116f3a5a128f24a5aa3fc749f78e80ae689e4
hiroshiy <fadilahsyafaat2427@gmail.com>
2023-08-09 19:05:21 +0800

    update left-side

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,51 @@
 // import { ThemeToggle } from '@/components/theme.toggle'
 import { siteConfig } from '@/config/site.config'
+import { MapPin } from 'lucide-react'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="flex flex-1 h-full w-full gap-10">
-      <div className="flex-1 p-6 h-full max-w-md">
+
+      <div className="flex-1 p-8 h-full max-w-md">
         <div className="flex flex-col h-full rounded-md space-y-6">
-          <img 
-            className="rounded-full" 
-            src="about.jpg" 
-            alt="raka-fadilah" 
-            width="90" 
-             height="90"
-            loading="eager"
+          <Image
+            className="rounded-lg"
+            alt="raka-fadilah"
+            src="/about.jpg"
+            width="90"
+            height="90"
+            priority
+            loading="eager" 
           />
+
           <div>
             <div className="text-xl text-primary font-semibold">
               {siteConfig.title}
             </div>
-            <div>
+            <div className="text-4xl font-bold mt-2">
               {siteConfig.creator}
             </div>
-            <div>
+            <div className="text-2xl font-light text-neutral-500">
               {siteConfig.bio}
             </div>
           </div>
+
+          <div className="flex items-center gap-6 justify-between text-sm">
+            <a className="border border-neutral-200 w-full gap-2 dark:border-neutral-800 py-2 px-3 rounded-md" href={siteConfig.locationLink}>
+              {siteConfig.location}
+            </a>
+            <a className="border border-neutral-200 w-full gap-2 dark:border-neutral-800 py-2 px-3 rounded-md" href={`mailto:${siteConfig.email}`}>
+              Contact Me
+            </a>
+          </div>
+          
+          <div>
+            <div></div>
+          </div>
         </div>
       </div>
+
       <div className="flex-1 p-6 h-full">
         <div className="flex items-center justify-center h-full rounded-md dark:bg-emerald-700">
           Right Side
--- config/site.config.ts
+++ config/site.config.ts
@@ -3,6 +3,6 @@ export const siteConfig = {
     title: "Frontend Developer",
     bio: "Building Website and creating Application",
     location: "Berau, Indonesia",
-    locationLink: "https://goo.gl/maps/jdFYax1KvdpduGtS9",
+    locationLink: "https://www.google.com/maps/place/Berau+Regency,+East+Kalimantan/@0.9281352,116.4266221,6.38z/data=!4m6!3m5!1s0x320db9032df93133:0x2cea5b974756a685!8m2!3d2.2791966!4d118.0908588!16s%2Fm%2F03gzm4g?entry=ttu",
     email: "fadilahsyafaat2427@gmail.com"
 } as const;
