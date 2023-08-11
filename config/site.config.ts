84f818ab332d2946ea8e5eae3a2da579add17b48
hiroshiy <fadilahsyafaat2427@gmail.com>
2023-08-11 15:59:09 +0800

    update siteconfig.bio

--- config/site.config.ts
+++ config/site.config.ts
@@ -21,7 +21,7 @@ export interface GridItemInterface {
 export const siteConfig = {
     creator: "Raka Fadilah",
     title: "Frontend Developer",
-    bio: "Building Website and creating Application",
+    bio: "Building Website and creating Mobile Apps.",
     location: "Berau, Indonesia",
     locationLink: "https://www.google.com/maps/place/Berau+Regency,+East+Kalimantan/@0.9281352,116.4266221,6.38z/data=!4m6!3m5!1s0x320db9032df93133:0x2cea5b974756a685!8m2!3d2.2791966!4d118.0908588!16s%2Fm%2F03gzm4g?entry=ttu",
     email: "fadilahsyafaat2427@gmail.com"
