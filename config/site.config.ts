30a323b656319b01ee2acd21681933a9dca6d158
hiroshiy <fadilahsyafaat2427@gmail.com>
2023-08-16 21:01:30 +0800

    feat: add new GridItems: GridItemInterface

--- config/site.config.ts
+++ config/site.config.ts
@@ -1,6 +1,6 @@
 export type GridItemLayout = "1x2" | "2x1" | "2x2" | "2x4";
 export type GridItemType = "social" | "music" | "project"
-export type EquipmentsItem = {
+export type PlaylistItem = {
     title: string;
     link: string;
 }
@@ -17,6 +17,8 @@ export interface GridItemInterface {
     buttonLink?: string;
     buttonSecondaryText?: string;
     stars?: number;
+    images?: string;
+    playlist?: PlaylistItem[];
 }
 
 const GridItems: GridItemInterface[] = [
@@ -54,7 +56,50 @@ const GridItems: GridItemInterface[] = [
         buttonTitle: "Connect",
         buttonLink: "https://linkedin.com/in/raka-fadilah",
         buttonSecondaryText: "40",
-    }
+    },
+    {
+        layout: "2x4",
+        type: "music",
+        title: "Spotify Playlist",
+        images: "/spotify-image.jpg",
+        playlist: [
+            {
+                title: "dopamine",
+                link: "https://open.spotify.com/playlist/48CZj6HS9YKADIHn39PR4C?si=553326b24ee245e0",
+            },
+            {
+                title: "WIB",
+                link: "https://open.spotify.com/playlist/4GGzjQX3mvbTdScQlCo1ig?si=02692d00a936489a",
+            },
+            {
+                title: "don't ask",
+                link: "https://open.spotify.com/playlist/3vHENPKkikygQUyFfAFw30?si=6e0c80f420ba452b",
+            },
+            {
+                title: "dm",
+                link: "https://open.spotify.com/playlist/5hSK72Co5w1pc7SmMrtjNp?si=99e924217ff6479e",
+            },
+            {
+                title: "昇る太陽の国",
+                link: "https://open.spotify.com/playlist/2eXE40MwtF6iqfHylMfpeP?si=1c35cba67b2a4a66",
+            },
+            {
+                title: "more emotional",
+                link: "https://open.spotify.com/playlist/7LmkifZ4YjFJ7XurEzI3xg?si=46d7a2124bd04032",
+            },
+        ],
+    },
+    {
+        layout: "2x2",
+        type: "social",
+        title: "Instagram",
+        icon: "instagram",
+        username: "@mosaicman_",
+        buttonTitle: "Follow",
+        buttonLink: "https://instagram.com/mosaicman_",
+        buttonSecondaryText: "111",
+        color: "#ff0000",
+    },
 ]
 
 export const siteConfig = {
