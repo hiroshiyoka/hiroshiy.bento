efa1265338471cd3388615a37fc3738399c374c0
hiroshiy <fadilahsyafaat2427@gmail.com>
2023-08-18 15:46:28 +0800

    feat: add variants

--- components/grid.item.tsx
+++ components/grid.item.tsx
@@ -1,6 +1,23 @@
 import { cn } from "@/utils/cn";
 import { cva, type VariantProps } from "class-variance-authority";
 
+const variants = cva (
+  "shadow-grid rounded-3xl bg-white dark:bg-neutral-900 flex flex-col justify-center", 
+  {
+    variants: {
+      size: {
+        "1x2": "col-span-1 row-span-2 p-8",
+        "2x1": "col-span-2 row-span-1 px-8 py-4",
+        "2x2": "col-span-2 row-span-2 p-8",
+        "2x4": "cols-span-2 row-span-4 relative overflow-hidden",
+      },
+    },
+    defaultVariants: {
+      size: "1x2",
+    },
+  }
+);
+
 const GridItems = ({ 
     size, 
     children 
