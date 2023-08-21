bdedf65c9c06d6271a1ec85a441c0f4ac5f31dd2
hiroshiy <fadilahsyafaat2427@gmail.com>
2023-08-21 14:19:24 +0800

    style: add some styling

--- components/grid-items/social.box.tsx
+++ components/grid-items/social.box.tsx
@@ -3,15 +3,15 @@ import React from 'react'
 
 const SocialBox = ({ item }: { item:GridItemInterface }) => {
   return (
-    <div>
+    <div className="flex items-center justify-between">
         <div>
             <div>ICON</div>
             { item.layout === "2x2" && <div>Button</div> }
         </div>
-        <div>
-            <div>{item.title}</div>
-            <div>{item.username}</div>
-            <div>{item.description}</div>
+        <div className="mt-2">
+            <div className="text-lg font-semibold">{item.title}</div>
+            <div className="text-sm text-neutral-500">{item.username}</div>
+            <div className="text-sm text-neutral-500">{item.description}</div>
         </div>
         {item.layout === '1x2' && <div>Button</div>}
     </div>
