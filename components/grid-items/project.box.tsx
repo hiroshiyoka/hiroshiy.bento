b9d54b859097783074cbe88e74d33b0146f0ecbb
hiroshiy <fadilahsyafaat2427@gmail.com>
2023-08-24 01:19:39 +0800

    fix: change div into next-link and add item color in Icon

--- components/grid-items/project.box.tsx
+++ components/grid-items/project.box.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Icon from "../icon";
 import { GridItemInterface } from "@/config/site.config";
+import Link from "next/link";
 
 const ProjectBox = ({ item }: { item: GridItemInterface }) => {
   return (
-    <div className="flex items-center gap-3">
-      <Icon type={item.icon ?? ""} />
+    <Link className="flex items-center gap-3" href={item.buttonLink ?? ""}>
+      <Icon type={item.icon ?? ""} color={item.color} />
       <div className="w-full text-lg font-semibold">{item.title}</div>
-    </div>
+    </Link>
   );
 };
 
