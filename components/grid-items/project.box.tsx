ad64cc2dda5cb7217aad7ab8f70b33f69d43ce97
hiroshiy <fadilahsyafaat2427@gmail.com>
2023-08-25 00:30:40 +0800

    feat: add github stars

--- components/grid-items/project.box.tsx
+++ components/grid-items/project.box.tsx
@@ -8,6 +8,9 @@ const ProjectBox = ({ item }: { item: GridItemInterface }) => {
     <Link className="flex items-center gap-3" href={item.buttonLink ?? ""}>
       <Icon type={item.icon ?? ""} color={item.color} />
       <div className="w-full text-lg font-semibold">{item.title}</div>
+      <div>
+        <span>{item.stars}</span>
+      </div>
     </Link>
   );
 };
