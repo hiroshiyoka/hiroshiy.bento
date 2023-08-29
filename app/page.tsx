ca854bcfb4b16746794b7837e054326cbeca83c2
hiroshiy <fadilahsyafaat2427@gmail.com>
2023-08-30 00:22:46 +0800

    style: delete some component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,3 @@
-import { LuMapPin, LuMail } from "react-icons/lu";
-import { ThemeToggle } from "@/components/theme.toggle";
-import { siteConfig } from "@/config/site.config";
-import Image from "next/image";
-import GridItems from "@/components/grid.item";
-import { title } from "process";
-import SocialBox from "@/components/grid-items/social.box";
-import ProjectBox from "@/components/grid-items/project.box";
-import SpotifyPlaylist from "@/components/grid-items/spotify.playlist";
-import Footer from "@/components/footer";
 import LeftSide from "@/components/left.side";
 import RightSide from "@/components/right.side";
 
