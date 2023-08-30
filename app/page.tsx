b0c0314415d93b66f4440e45e2b58d375b136a87
hiroshiy <fadilahsyafaat2427@gmail.com>
2023-08-31 00:40:17 +0800

    feat: add container and margin auto horizontal

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import RightSide from "@/components/right.side";
 
 export default function Home() {
   return (
-    <main className="flex flex-col xl:flex-row flex-1 h-full w-full gap-10">
+    <main className="container flex flex-col xl:flex-row flex-1 h-full w-full mx-auto gap-10">
       <LeftSide />
       <RightSide />
       <div className="flex xl:hidden px-8 pb-10 w-full">
