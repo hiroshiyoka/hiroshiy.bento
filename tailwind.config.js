db060cb7b6a7f71250f10344de56e35fd541d863
hiroshiy <fadilahsyafaat2427@gmail.com>
2023-09-01 01:16:24 +0800

    chore: add @tailwindcss/container-queries to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-      "./components/**/*.{js,ts,jsx,tsx,mdx}",
-      "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    ],
-    darkMode : ["class"],
-    theme: {
-      extend: {
-        colors: {
-          primary: "#6178F1",
-          youtube: "#FF0000",
-          twitter: "#1DA1F2",
-          github: "#181717",
-        },
-        boxShadow: {
-          grid: "0px 4px 10px 0px rgba(0, 0, 0, 0.05);",
-        },
-        fontFamily: {
-          sans: ["var(--font-sfpro)"],
-        },
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  darkMode: ["class"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#6178F1",
+        youtube: "#FF0000",
+        twitter: "#1DA1F2",
+        github: "#181717",
+      },
+      boxShadow: {
+        grid: "0px 4px 10px 0px rgba(0, 0, 0, 0.05);",
+      },
+      fontFamily: {
+        sans: ["var(--font-sfpro)"],
       },
     },
-    plugins: [],
+  },
+  plugins: [require("@tailwindcss/container-queries")],
 };
