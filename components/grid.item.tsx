e0281a4fa21bec60347c24f4f69db63ad004fa4c
hiroshiy <fadilahsyafaat2427@gmail.com>
2023-09-01 01:20:59 +0800

    feat: add responsive grid columns and change variants size

--- components/grid.item.tsx
+++ components/grid.item.tsx
@@ -2,15 +2,15 @@ import { cn } from "@/utils/cn";
 import { cva, type VariantProps } from "class-variance-authority";
 import React from "react";
 
-const variants = cva (
-  "shadow-grid rounded-3xl bg-white dark:bg-neutral-900 flex flex-col justify-center", 
+const variants = cva(
+  "shadow-grid rounded-3xl bg-white dark:bg-neutral-900 flex flex-col justify-center",
   {
     variants: {
       size: {
         "1x2": "col-span-1 row-span-2 p-8",
-        "2x1": "col-span-2 row-span-1 px-8 py-4",
-        "2x2": "col-span-2 row-span-2 p-8",
-        "2x4": "col-span-2 row-span-4 relative overflow-hidden",
+        "2x1": "col-span-1 md:col-span-2 row-span-1 px-8 py-4",
+        "2x2": "col-span-1 md:col-span-2 row-span-2 p-8",
+        "2x4": "col-span-1 md:col-span-2 row-span-4 relative overflow-hidden",
       },
     },
     defaultVariants: {
@@ -19,14 +19,16 @@ const variants = cva (
   }
 );
 
-type GridItemsProps = {children: React.ReactNode} & VariantProps<typeof variants>;
+type GridItemsProps = { children: React.ReactNode } & VariantProps<
+  typeof variants
+>;
 
 const GridItems = ({ size, children }: GridItemsProps) => {
-  return ( 
+  return (
     <div className={cn(variants({ size, className: "bg-emerald-100" }))}>
       {children}
     </div>
   );
 };
 
-export default GridItems
+export default GridItems;
