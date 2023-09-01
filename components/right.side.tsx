6541b4ec3037c54cc445b26a894bf84af9e33822
hiroshiy <fadilahsyafaat2427@gmail.com>
2023-09-02 00:58:22 +0800

    feat: change the gap

--- components/right.side.tsx
+++ components/right.side.tsx
@@ -8,7 +8,7 @@ import SpotifyPlaylist from "./grid-items/spotify.playlist";
 const RightSide = () => {
   return (
     <div className="flex-1 md:h-full">
-      <div className="w-full md:h-full p-6 xl:overflow-y-auto grid grid-cols-2 md:grid-cols-4 auto-rows-[76px] gap-10">
+      <div className="w-full md:h-full p-6 xl:overflow-y-auto grid grid-cols-2 md:grid-cols-4 auto-rows-[76px] gap-6 xl:gap-10">
         {siteConfig.items.map((item, index) => {
           return (
             <GridItems key={item.title + index} size={item.layout}>
