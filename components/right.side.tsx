6b1dd73af50de30b914ebb0e99addd98a36f3324
hiroshiy <fadilahsyafaat2427@gmail.com>
2023-11-10 21:54:40 +0700

    feat: add scope function with useEffect

--- components/right.side.tsx
+++ components/right.side.tsx
@@ -7,15 +7,38 @@ import SocialBox from "./grid-items/social.box";
 import ProjectBox from "./grid-items/project.box";
 import SpotifyPlaylist from "./grid-items/spotify.playlist";
 import { useAnimate, stagger } from "framer-motion";
+import { useEffect } from "react";
 
 const staggerDelay = stagger(0.1);
 
 const RightSide = () => {
   const [scope, animate] = useAnimate();
 
+  useEffect(() => {
+    if (scope.current) {
+      animate(
+        "div",
+        {
+          opacity: 1,
+          y: 0,
+          scale: 1,
+        },
+        {
+          type: "spring",
+          stiffness: 330,
+          damping: 35,
+          delay: staggerDelay,
+        }
+      );
+    }
+  }, [scope]);
+
   return (
     <div className="flex-1 md:h-full">
-      <div className="w-full md:h-full p-6 xl:overflow-y-auto grid grid-cols-2 md:grid-cols-4 auto-rows-[76px] gap-6 xl:gap-10">
+      <div
+        ref={scope}
+        className="w-full md:h-full p-6 xl:overflow-y-auto grid grid-cols-2 md:grid-cols-4 auto-rows-[76px] gap-6 xl:gap-10"
+      >
         {siteConfig.items.map((item, index) => {
           return (
             <GridItems key={item.title + index} size={item.layout}>
