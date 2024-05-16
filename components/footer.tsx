c188fed2cd2f4f750a4e84621f4c1e214d3dbd6d
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-05-16 17:55:48 +0700

    feat(footer): update footer copyright info

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className="border-t w-full border-neutral-200 dark:border-neutral-800 pt-6 text-xs flex items-center justify-between">
       <div className="text-neutral-500">
-        Built by @hiroshiy | ©2023 hiroshiyoka. All Rights Reserved
+        Built by @hiroshiy | ©2024 hiroshiyoka. All Rights Reserved
       </div>
       <ThemeToggle />
     </div>
