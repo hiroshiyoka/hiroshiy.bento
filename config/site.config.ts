50953f6a88b217a93baf63b7950a459f21d9fa78
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-05-16 17:52:54 +0700

    feat(site-config): update website bio information

--- config/site.config.ts
+++ config/site.config.ts
@@ -122,7 +122,7 @@ const GridItems: GridItemInterface[] = [
 export const siteConfig = {
   creator: "Raka Fadilah",
   title: "Frontend Developer",
-  bio: "Building Website and Creating User Interface Design",
+  bio: "Building Website and Mobile Apps",
   location: "Berau, Indonesia",
   locationLink:
     "https://www.google.com/maps/place/Berau+Regency,+East+Kalimantan/@0.9281352,116.4266221,6.38z/data=!4m6!3m5!1s0x320db9032df93133:0x2cea5b974756a685!8m2!3d2.2791966!4d118.0908588!16s%2Fm%2F03gzm4g?entry=ttu",
